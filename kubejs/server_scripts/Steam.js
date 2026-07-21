90f9a3777b3f11d4c83636ca09f60f82f4fc844e
NemEzanevem <patrikborka65@gmail.com>
2026-07-21 22:49:22 +0200

    aa

--- kubejs/server_scripts/Steam.js
+++ kubejs/server_scripts/Steam.js
@@ -1,62 +1,97 @@
 ServerEvents.recipes(event => {
-event.shaped(
-  Item.of('wfcore:detonator'), 
-  [
-    'ACA',
-    'BDB', 
-    'ACA'
-],
-  {
-    A: 'gtceu:wrought_iron_screw',
-    B: 'gtceu:iron_spring', 
-    C: 'gtceu:red_alloy_single_wire',
-    D: 'minecraft:stone_button'
-  }
-)
+  event.shaped(
+    Item.of('wfcore:detonator'),
+    [
+      'ACA',
+      'BDB',
+      'ACA'
+    ],
+    {
+      A: 'gtceu:wrought_iron_screw',
+      B: 'gtceu:iron_spring',
+      C: 'gtceu:red_alloy_single_wire',
+      D: 'minecraft:stone_button'
+    }
+  )
 
 
-event.shaped(
-  Item.of('wfcore:mining_charge'), 
-  [
-    'ABA',
-    'BBB', 
-    'ABA'
-],
-  {
-    A: 'minecraft:paper',
-    B: 'minecraft:gunpowder', 
+  event.shaped(
+    Item.of('wfcore:mining_charge'),
+    [
+      'ABA',
+      'BBB',
+      'ABA'
+    ],
+    {
+      A: 'minecraft:paper',
+      B: 'minecraft:gunpowder',
 
-  }
-)
+    }
+  )
 
-event.shaped(
-  Item.of('wfcore:deep_mining_charge'), 
-  [
-    'ACA',
-    'CBC', 
-    'ACA'
-],
-  {
-    A: 'minecraft:paper',
-    B: 'minecraft:gunpowder',
-    C: 'wfcore:mining_charge'
+  event.shaped(
+    Item.of('wfcore:deep_mining_charge'),
+    [
+      'ACA',
+      'CBC',
+      'ACA'
+    ],
+    {
+      A: 'minecraft:paper',
+      B: 'minecraft:gunpowder',
+      C: 'wfcore:mining_charge'
 
-  }
-)
+    }
+  )
 
-event.shaped(
-  Item.of('wfcore:steam_wiremill'), 
-  [
-    'ADA',
-    'CBC', 
-    'ADA'
-],
-  {
-    A: 'gtceu:bronze_machine_casing',
-    B: 'gtceu:bronze_gearbox',
-    C: 'gtceu:vacuum_tube',
-    D: 'gtceu:bronze_frame'
+  event.shaped(
+    Item.of('wfcore:steam_wiremill'),
+    [
+      'ADA',
+      'CBC',
+      'ADA'
+    ],
+    {
+      A: 'gtceu:bronze_machine_casing',
+      B: 'gtceu:bronze_gearbox',
+      C: 'gtceu:vacuum_tube',
+      D: 'gtceu:bronze_frame'
 
-  }
-)
+    }
+  )
+
+  event.shaped(
+    Item.of('wfcore:foundry_basin'),
+    [
+      'A A',
+      'A A',
+      'BBB'
+    ],
+    {
+
+      A: 'gtceu:firebricks',
+      B: 'gtceu:steel_plate',
+
+    }
+  )
+
+
+  event.shaped(
+    Item.of('wfcore:foundry_mold_caster'),
+    [
+      '   ',
+      'A A',
+      'BBB'
+    ],
+    {
+
+      A: 'gtceu:firebricks',
+      B: 'gtceu:steel_plate',
+
+    }
+  )
+
+  event.remove({ id: 'wfcore:shaped/foundry_basin' })
+  event.remove({ id: 'wfcore:shaped/foundry_mold_caster' })
+
 })
